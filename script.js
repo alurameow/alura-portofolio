12d986c248634d24915391d473e6702cad6c67da
alurameow <lillydt2007@gmail.com>
2025-07-07 03:30:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 function toggleMenu() {
-  const navLinks = document.getElementById("nav-links");
-  navLinks.classList.toggle("active");
+  const navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('active');
 }
